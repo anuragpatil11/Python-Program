baf5a903ec6f5cf562426e6ed156d0b9c949130d
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2023-09-26 00:44:54 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+a=int(input("Enter first Number: ")) # taking first number as a input from user
+b=int(input("Enter second number:")) # taking second number as a input from user
+c=(a+b) # addtion opertor
+print ("The sum of two numbers is:", c )
+d=a-b # subtraction operator
+print("The division of two number is",d)
+e=a*b # multipication operator
+print('the multiplication of the two number', e)
+f= a / b  #division operator
+print ("the divison of the two number:", f) 
+g=a//b  # floor division operator
+print("The floor division of two number is",g)
+h=(a%b)
+print ('remainder of first and Second number is:', h)
+
+"""
+    output
+Enter first Number: 22
+Enter second number:2
+The sum of two numbers is: 24
+The division of two number is 20
+the multiplication of the two number 44
+the divison of the two number: 11.0
+The floor division of two number is 11
+remainder of first and Second number is: 0
+"""
