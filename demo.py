2c76c6d08f934caea84585ae4e371d862181d603
anuragpatil_11 <113289237+anuragpatil11@users.noreply.github.com>
2024-01-14 14:57:20 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,16 @@
+'''a=int(input("a:"));
+b=int(input("b:"));
+c=a+b;
+print(c);'''
+print("hello")
+print("hey i am a \"good boy\" \n and this viewer is also a good boygirl")
+
+# multiline string
+s1= '''hi anurag
+how r u good na 
+i m fine'''
+
+print(s1)
+
+s2="harry"
+print(s2[-4:-2])
---
+++ first.py
@@ -0,0 +1,15 @@
+# print("hello,world")
+# print(5)
+a="1"
+b="43"
+# Explicit typecasting because it is converted by the user or programmer
+c=int(a)+int(b) # type casting means converting a string data type into an integer data type using "int()" function or method
+print(c)
+
+# impicit typecasting
+d=1.1
+e=6
+print(d+e) #implicit type casting is here because compiler convert an intger into float and give an output as float number
+print(d,type(d))
+print(e,type(e))
+print(type(d+e))
